5be217d31acbe4cf78e07506a98b1bae13567024
ryanscharf <ryan.scharf@gmail.com>
2016-10-11 16:27:37 -0400

    added hybrid values

--- table 1.R
+++ table 1.R
@@ -1,36 +1,41 @@
-reeetweets <- fl %>% group_by(text) %>% summarise(retweet_count = max(retweet_count))
-reeetweets[reeetweets == 0] <- NA
-reeetweets<- na.omit(reeetweets)
+#reeetweets <- zika1 %>% group_by(text) %>% summarise(retweet_count = max(retweet_count))
+#reeetweets[reeetweets == 0] <- NA
+#reeetweets<- na.omit(reeetweets)
 
 
-##
-#number of unique text only tweets
-bss <- fl[is.na(fl[,7]) & is.na(fl[,26]),]
+#number of  text only tweets
+bss <- zika1[is.na(zika1[,7]) & is.na(zika1[,26]),]
 #number of unique text only agents
 length(unique(bss$screen_name))
 #numberb ofo unique text only coutnries
 length(unique(bss$country))
 
-##
-#number of unique url only tweets
-bss <- fl[is.na(fl[,7]) & !is.na(fl[,26]),]
+#number of url only tweets
+bss <- zika1[is.na(zika1[,7]) & !is.na(zika1[,26]),]
+length(bss[,1])
 #number of unique url only agents
 length(unique(bss$screen_name))
 #numberb ofo unique url only coutnries
 length(unique(bss$country))
 
-##
-#number of unique, photo only tweets
-bss <- fl[grep("photo/1", fl$parsed_media_url),]
+#number of tweets that have photos embedded
+bss <- zika1[grep("photo/1", zika1$parsed_media_url),]
+length(bss[,1])
 #number of unique photo only agents
 length(unique(bss$screen_name))
 #number of unique photo only countries
 length(unique(bss$country))
 
-##
-#number of unique, video only tweets
-bss <- fl[grep("video/1", fl$parsed_media_url),]
+#number of tweets with videos embedded
+bss <- zika1[grep("video/1", zika1$parsed_media_url),]
+length(bss[,1])
 #number of unique video only agents
 length(unique(bss$screen_name))
 #number of unique video only countries
+length(unique(bss$country))
+
+#get numbers for mixed things
+bss <- zika1[!is.na(zika1[,7]) & !is.na(zika1[,26]),]
+length(bss[,1])
+length(unique(bss$screen_name))
 length(unique(bss$country))
